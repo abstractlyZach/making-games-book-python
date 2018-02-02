2118e4bd2551d69fe2fc40bbf52379cebc5e2681
Zachary Lee <zach3lee@gmail.com>
2018-02-01 21:05:20 -0800

    Refactor sanity checks to throw exceptions

--- memory_puzzle/memory_puzzle/settings.py
+++ memory_puzzle/memory_puzzle/settings.py
@@ -12,12 +12,19 @@ BOARD_HEIGHT = 7 # height in # of boxes
 X_MARGIN = int((WINDOW_WIDTH - (BOARD_WIDTH * (BOX_SIZE + GAP_SIZE))) / 2)
 Y_MARGIN = int((WINDOW_HEIGHT- (BOARD_HEIGHT * (BOX_SIZE + GAP_SIZE))) / 2)
 
-assert(BOARD_HEIGHT * BOARD_WIDTH % 2 == 0,
-       'Board needs to have an even number of boxes for pairs of matches.')
 
-assert(len(constants.ALL_COLORS) * len(constants.ALL_SHAPES) * 2 >=
-       BOARD_WIDTH * BOARD_HEIGHT,
-       "Board is too big for the number of shapes/colors defined.")
+# sanity checks
+if BOARD_HEIGHT * BOARD_WIDTH % 2 != 0:
+    error_message = 'Board needs to have an even number of boxes for pairs ' \
+                    'of matches.'
+    raise Exception(error_message)
+
+if len(constants.ALL_COLORS) * len(constants.ALL_SHAPES) * 2 < \
+       BOARD_WIDTH * BOARD_HEIGHT:
+       error_message = 'Board is too big for the number of shapes/colors ' \
+                        'defined.'
+       raise Exception(error_message)
+
 
 BG_COLOR = constants.NAVYBLUE
 LIGHT_BG_COLOR = constants.GRAY
