09ecb0aadf0c1544ffe3b2ca14edad26f3f86df3
Zachary Lee <zach3lee@gmail.com>
2018-04-25 15:12:10 -0700

    Add docstrings to view and controller

--- memory_puzzle/memory_puzzle/controller.py
+++ memory_puzzle/memory_puzzle/controller.py
@@ -5,19 +5,22 @@ from . import events
 
 
 class Controller(object):
-    """Handles input by posting events to the event manager when input
-    happens."""
+    """Converts low-level input events and converts them into events that
+    are easier for the game to understand."""
     def __init__(self, event_manager, model):
         self._event_manager = event_manager
         self._event_manager.register_listener(self)
         self._model = model
 
     def notify(self, event):
+        """Gathers low-level input events and posts higher-level input
+        events to the event manager."""
         if isinstance(event, events.TickEvent):
             for input_event in pygame.event.get():
                 self._handle_event(input_event)
 
     def _handle_event(self, event):
+        """Processes events and posts the proper event, if necessary."""
         if self._is_quit_event(event):
             event_to_post = events.QuitEvent()
         elif (event.type == pygame.KEYUP):
@@ -33,6 +36,7 @@ class Controller(object):
         self._event_manager.post(event_to_post)
 
     def _is_quit_event(self, event):
+        """Returns True if the event should signify a 'quit event'."""
         if event.type == pygame.QUIT:
             return True
         elif (event.type == pygame.KEYUP) and \
--- memory_puzzle/memory_puzzle/view.py
+++ memory_puzzle/memory_puzzle/view.py
@@ -38,6 +38,7 @@ class GraphicalView(object):
         logging.info('View initialized.')
 
     def notify(self, event):
+        """Handle an event from the event manager."""
         if isinstance(event, events.TickEvent):
             self._handle_tick_event()
         elif isinstance(event, events.MouseMovementEvent):
@@ -58,6 +59,7 @@ class GraphicalView(object):
             pygame.quit()
 
     def _handle_tick_event(self):
+        """Handle a tick event."""
         if self._animation_statuses.animations_paused:
             self._handle_pause_tick()
         else:
@@ -65,16 +67,20 @@ class GraphicalView(object):
         self.render_all()
 
     def _handle_pause_tick(self):
+        """Handle a single tick while animations are paused."""
         self._frames_left_until_unpause -= 1
         if self._frames_left_until_unpause <= 0:
             self._animation_statuses.unpause_animations()
 
     def _progress_animations(self):
+        """Handle a single tick of animation."""
         self._handle_animation_queue()
         for animation in self._animation_statuses.get_active_animations():
             animation.tick_animation()
 
     def _handle_animation_queue(self):
+        """Gather animation requests and fulfill them until the queue is
+        empty or a pause event is found."""
         while not len(self._animation_request_queue) <= 0:
             request = self._animation_request_queue.pop(0)
             if isinstance(request, events.AnimationPause):
@@ -84,6 +90,8 @@ class GraphicalView(object):
                 self._handle_box_animation_request(request)
 
     def _handle_pause_request(self, request):
+        """If animations are active, defer the pause until they're done.
+        Otherwise, pause all the animations."""
         if self._animation_statuses.any_animations_active():
             self._animation_request_queue.insert(0, request)
         else:
@@ -92,6 +100,7 @@ class GraphicalView(object):
                 request.seconds * settings.FPS
 
     def _handle_box_animation_request(self, request):
+        """Sort a box animation request into its proper handling function."""
         if isinstance(request, events.BoxCloseRequest):
             self._handle_box_close_request(request)
         elif isinstance(request, events.BoxOpenRequest):
@@ -100,6 +109,7 @@ class GraphicalView(object):
             self._handle_box_open_and_close_request(request)
 
     def render_all(self):
+        """Draw the current frame to the display."""
         if not self._is_initialized:
             return
         # draw stuff
@@ -107,22 +117,27 @@ class GraphicalView(object):
         self._draw_visible_icons()
         self._draw_box_covers()
         self._try_to_draw_highlight(self._mouse_position)
+        # draw visual debug stuff
         if settings.VISUAL_DEBUG_MODE:
             self._draw_guidelines(constants.RED)
             self._draw_click_markers()
+        # send frame to the display
         pygame.display.update()
 
     def _draw_visible_icons(self):
+        """Draw all icons that are visible."""
         for coord in coords.get_all_box_coords():
             animation_status = self._animation_statuses.get_status(coord)
             if animation_status.icon_visible:
                 self._draw_icon_at_coord(coord)
 
     def _draw_icon_at_coord(self, coord):
+        """Draw the icon at the given coordinate."""
         icon = self._model.get_icon(coord)
         icon.draw(self._display_surface)
 
     def _draw_box_covers(self):
+        """Draw all box covers."""
         for coord in coords.get_all_box_coords():
             if self._animation_statuses.any_animations_active():
                 animation_status = self._animation_statuses.get_status(coord)
@@ -132,6 +147,8 @@ class GraphicalView(object):
                     self._draw_box_cover(coord, settings.BOX_SIZE)
 
     def _draw_box_cover(self, box_coords, coverage):
+        """Draw the box cover at the given coordinate. Coverage is the
+        number of pixels of width that this cover is using to cover its box."""
         if coverage > settings.BOX_SIZE:
             raise Exception
         topleft_corner = coords.top_left_coords_of_box(box_coords)
@@ -142,6 +159,7 @@ class GraphicalView(object):
                              rect_tuple)
 
     def _draw_guidelines(self, color):
+        """Draw guidelines so you can see where the margins are."""
         top_line = settings.Y_MARGIN
         bottom_line = settings.WINDOW_HEIGHT - settings.Y_MARGIN
         left_line = settings.X_MARGIN
@@ -160,10 +178,12 @@ class GraphicalView(object):
                          (right_line, settings.WINDOW_HEIGHT))
 
     def _draw_click_markers(self):
+        """Draw markers that denote where the last few clicks were."""
         for click_coord in self._clicks:
             self._draw_x(click_coord, constants.GREEN)
 
     def _draw_x(self, coord, color):
+        """Draw an 'X' at the given coordinates."""
         edge_distance = 10
         top_left = (coord.pixel_x - edge_distance,
                     coord.pixel_y - edge_distance)
@@ -177,11 +197,13 @@ class GraphicalView(object):
         pygame.draw.line(self._display_surface, color, top_right, bottom_left)
 
     def _handle_click(self, click_coords):
+        """Record the click in memory."""
         if len(self._clicks) >= 3:
             self._clicks.pop(0)
         self._clicks.append(click_coords)
 
     def _handle_box_open_and_close_request(self, event):
+        """Handle a BoxOpenAndCloseRequest."""
         current_status = self._animation_statuses.get_status(event.coords)
         should_open = not current_status.being_animated
         should_open = should_open and not current_status.icon_visible
@@ -189,6 +211,7 @@ class GraphicalView(object):
             self._open_and_close_box(event.coords)
 
     def _handle_box_open_request(self, event):
+        """Handle a BoxOpenRequest."""
         current_status = self._animation_statuses.get_status(event.coords)
         should_open = not current_status.being_animated
         should_open = should_open and not current_status.icon_visible
@@ -197,6 +220,7 @@ class GraphicalView(object):
             self._event_manager.post(events.BoxOpenConfirm(event.coords))
 
     def _handle_box_close_request(self, event):
+        """Handle a BoxCloseRequest."""
         current_status = self._animation_statuses.get_status(event.coords)
         should_close = not current_status.being_animated
         should_close = should_close and current_status.icon_visible
@@ -205,21 +229,26 @@ class GraphicalView(object):
             self._event_manager.post(events.BoxCloseConfirm(event.coords))
 
     def _open_and_close_box(self, coord):
+        """Open and close a box."""
         animation_target = self._animation_statuses.get_status(coord)
         if not animation_target.being_animated:
             animation_target.start_animation(-4, True)
 
     def _open_box(self, coord):
+        """Open a box."""
         animation_target = self._animation_statuses.get_status(coord)
         if not animation_target.being_animated:
             animation_target.start_animation(-4)
 
     def _close_box(self, coord):
+        """Close a box."""
         animation_target = self._animation_statuses.get_status(coord)
         if not animation_target.being_animated:
             animation_target.start_animation(4)
 
     def _do_new_game_animation(self):
+        """Drop animation requests onto the queue for the new game
+        animation."""
         all_coords = coords.get_all_box_coords()
         random.shuffle(all_coords)
         for i in range(0, len(all_coords), REVEAL_GROUPS):
@@ -229,10 +258,13 @@ class GraphicalView(object):
             self._animation_request_queue.append(events.AnimationPause(0))
 
     def _try_to_draw_highlight(self, coord):
+        """Attempt to draw a highlight at the coordinate to show where the
+        mouse is."""
         if coord.in_a_box:
             self._draw_highlight(coord)
 
     def _draw_highlight(self, coord):
+        """Draw a highlight around the box at the given coord."""
         top_left = coords.top_left_coords_of_box(coord)
         highlight_thickness = math.floor(settings.GAP_SIZE / 2)
         highlight_width = settings.BOX_SIZE + settings.GAP_SIZE
@@ -243,4 +275,5 @@ class GraphicalView(object):
                          bounding_rect, highlight_thickness)
 
     def _handle_game_over(self):
+        """Handle the game over event."""
         pass
