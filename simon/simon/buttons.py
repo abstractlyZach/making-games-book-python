a03be34354e92321ab2fd584d2bb6bfc442b5006
Zachary Lee <zach3lee@gmail.com>
2018-05-26 10:37:14 -0700

    Create button state enum

--- simon/simon/buttons.py
+++ simon/simon/buttons.py
@@ -1,9 +1,21 @@
 
+import enum
+
+class ButtonState(enum.Enum):
+    BRIGHTENING = 0
+    DARKENING = 1
+    IDLE = 2
+
+
+
 class Button(object):
     def __init__(self, original_color, flash_color):
         self._original_color = original_color
         self._flash_color = flash_color
-        self._brightening = False
+        self._state = ButtonState.IDLE
 
     def update(self):
-        if self._brightening:
+        if self._state is ButtonState.DARKENING:
+            pass
+        elif self._state is ButtonState.BRIGHTENING:
+            pass
