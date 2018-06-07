053ba28ea96ccc3403e4f2a251f352e32338b788
Zach Lee <zach.lee@concur.com>
2018-06-07 06:32:32 -0700

    Add underscores for consistency

--- wormy/wormy/constants.py
+++ wormy/wormy/constants.py
@@ -3,8 +3,8 @@ WHITE     = (255, 255, 255)
 BLACK     = (  0,   0,   0)
 RED       = (255,   0,   0)
 GREEN     = (  0, 255,   0)
-DARKGREEN = (  0, 155,   0)
-DARKGRAY  = ( 40,  40,  40)
+DARK_GREEN = (0, 155, 0)
+DARK_GRAY  = (40, 40, 40)
 
 UP = 'up'
 DOWN = 'down'
