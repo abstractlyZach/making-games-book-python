033c1774c26efc135a7ffe55328f45a95d083841
Zach Lee <zach.lee@concur.com>
2018-06-07 06:32:06 -0700

    Add constants and settings

--- wormy/wormy/settings.py
+++ wormy/wormy/settings.py
@@ -1,1 +1,12 @@
-FPS = 30
+from . import constants
+
+
+FPS = 15
+BG_COLOR = constants.BLACK
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+CELL_SIZE = 20
+assert WINDOW_WIDTH % CELL_SIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOW_HEIGHT % CELL_SIZE == 0, "Window height must be a multiple of cell size."
+CELL_WIDTH = WINDOW_WIDTH / CELL_SIZE
+CELL_HEIGHT = WINDOW_HEIGHT / CELL_SIZE
