d4366653833d9bd3e3e18e68aa29ea0fcedebea7
Zach Lee <zach.lee@concur.com>
2018-06-08 02:16:09 -0700

    Create coordinates

---
+++ wormy/wormy/coordinates.py
@@ -0,0 +1,19 @@
+from . import settings
+
+
+class Coordinates(object):
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+        self._check_in_bounds()
+
+    def _check_in_bounds(self):
+        x_in_bounds = self._x >= 0 or \
+            self._x <= settings.WINDOW_WIDTH / settings.CELL_SIZE
+        y_in_bounds = self._y >= 0 or \
+            self._y <= settings.WINDOW_HEIGHT / settings.CELL_SIZE
+        if x_in_bounds and y_in_bounds:
+            return True
+        else:
+            raise Exception(f'Coordinates({self._x}, {self._y}) are out of '
+                            f'bounds')
